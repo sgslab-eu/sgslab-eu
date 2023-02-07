6c73938fe1f187d3464ae37d6a52b593ebe2366d
sgslab-eu <124488523+sgslab-eu@users.noreply.github.com>
2023-02-07 12:58:28 +0000

    added content.js

---
+++ src/js/content.js
@@ -0,0 +1,70 @@
+const htmlContent = `
+    <section class="header">
+        <h2 class="title">SGS DCC</h2>
+        <p>
+            SGS Document Change Controller
+        </p>
+    </section>
+    <div class="page-section">
+        <h2>Document Detail</h2>
+        <div class="change-box">
+            <label for="page_id">PageId</label>
+            <input id="page_id" type="text" readonly>
+
+            <label for="product_name">Name of Product</label>
+            <input id="product_name" type="text" readonly>
+            
+            <label for="desc">Description</label>
+            <input id="desc" type="text" readonly>
+
+            <label for="pack_size">Pack Size</label>
+            <input id="pack_size" type="text" readonly></input>
+            
+            <label for="stability">Stability</label>
+            <input id="stability" type="text" readonly></input>
+
+            <label for="storage_conditions">Storage Conditions</label>
+            <input id="storage_conditions" type="text" readonly></input>
+
+            <label for="country">Country</label>
+            <input id="country" type="text" readonly></input>
+
+            <label for="procedure_number">Procedure #</label>
+            <input id="procedure_number" type="text" readonly></input>
+
+            <label for="ma_number">MA #</label>
+            <input id="ma_number" type="text" readonly></input>
+
+            <label for="ma_grant_date">MA Grant Date</label>
+            <input id="ma_grant_date" type="text" readonly></input>
+
+            <label for="ma_renewal_date">MA Renewal Date</label>
+            <input id="ma_renewal_date" type="text" readonly></input>
+
+
+            <label for="remarks">Remarks</label>
+            <textarea id="remarks" readonly></textarea>
+        </div>
+    </div>
+
+    <div class="page-section">
+        <h2>Files Attached</h2>
+        <div class="change-box">
+            <pre><code id="file_list" style="background-color:white; padding: 20px;"></code></pre>
+            
+            <div style="width: 100%; display: flex; align-items: flex-end; flex-direction: column;">
+                <input id="change" class="button-primary" type="button" style="width: 180px;"  value="Change Request" />
+            </div>
+        </div>
+    </div>
+
+    <div class="page-section">
+        <h3>Current Login</h3>
+        <pre><code id="user" style="overflow: scroll;"></code></pre>
+        <h3>Data</h3>
+        <pre><code id="data" style="overflow: scroll;"></code></pre>
+        <h3>Result</h3>
+        <pre><code id="result" style="overflow: scroll;"></code></pre>
+    </div>`;
+
+    export {htmlContent};
--- src/js/script.js
+++ src/js/script.js
@@ -2,76 +2,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.0/firebase-app.js'
 import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.17.0/firebase-auth.js'
 
-const htmlContent = `
-    <section class="header">
-        <h2 class="title">SGS Lab</h2>
-        <p>
-            SGS Document Change Controller
-        </p>
-    </section>
-    <div class="page-section">
-        <h2>Document Detail</h2>
-        <div class="change-box">
-            <label for="page_id">PageId</label>
-            <input id="page_id" type="text" readonly>
-
-            <label for="product_name">Name of Product</label>
-            <input id="product_name" type="text" readonly>
-            
-            <label for="desc">Description</label>
-            <input id="desc" type="text" readonly>
-
-            <label for="pack_size">Pack Size</label>
-            <input id="pack_size" type="text" readonly></input>
-            
-            <label for="stability">Stability</label>
-            <input id="stability" type="text" readonly></input>
-
-            <label for="storage_conditions">Storage Conditions</label>
-            <input id="storage_conditions" type="text" readonly></input>
-
-            <label for="country">Country</label>
-            <input id="country" type="text" readonly></input>
-
-            <label for="procedure_number">Procedure #</label>
-            <input id="procedure_number" type="text" readonly></input>
-
-            <label for="ma_number">MA #</label>
-            <input id="ma_number" type="text" readonly></input>
-
-            <label for="ma_grant_date">MA Grant Date</label>
-            <input id="ma_grant_date" type="text" readonly></input>
-
-            <label for="ma_renewal_date">MA Renewal Date</label>
-            <input id="ma_renewal_date" type="text" readonly></input>
-
-
-            <label for="remarks">Remarks</label>
-            <textarea id="remarks" readonly></textarea>
-        </div>
-    </div>
-
-    <div class="page-section">
-        <h2>Files Attached</h2>
-        <div class="change-box">
-            <pre><code id="file_list" style="background-color:white; padding: 20px;"></code></pre>
-            
-            <div style="width: 100%; display: flex; align-items: flex-end; flex-direction: column;">
-                <input id="change" class="button-primary" type="button" style="width: 180px;"  value="Change Request" />
-            </div>
-        </div>
-    </div>
-
-    <div class="page-section">
-        <h3>Current Login</h3>
-        <pre><code id="user" style="overflow: scroll;"></code></pre>
-        <h3>Data</h3>
-        <pre><code id="data" style="overflow: scroll;"></code></pre>
-        <h3>Result</h3>
-        <pre><code id="result" style="overflow: scroll;"></code></pre>
-    </div>`;
-
-
 function initialize(config){
     const app = initializeApp( config );
     const auth = getAuth(app);
@@ -90,8 +20,49 @@ function getToken(userCredentials){
         .catch( (error) => {throw error} );
 }
 
-function getData(doc_id, baseUrl, token){
-    let URL = baseUrl + '/documents/' + doc_id + '.json?auth=' + token;
+
+async function renderPage(config, page_id, spaceKey, window) {
+
+    const auth = await initialize(config['firebase']);
+    $('#result').html(`<b>Done:</b> initialization...`) ;
+    
+    const credentials = await signin(auth, config['signin']['username'], config['signin']['password']);
+    $('#result').html(`<b>Done:</b> fetched credentials...`) ;
+    
+    const token = await getToken(credentials);
+    $('#result').html(`<b>Done:</b> fetched token...`) ;
+
+    const data = await getData(page_id, spaceKey, config['firebase']['databaseURL'], token );
+            
+    $('#product_name').val(data['product_name']);
+    $('#desc').val(data['desc']);
+    $('#pack_size').val(data['pack_size']);
+    $('#stability').val(data['stability']);
+    $('#storage_conditions').val(data['storage_conditions']);
+    $('#country').val(data['country']);
+    $('#procedure_number').val(data['procedure_number']);
+    $('#ma_number').val(data['ma_number']);
+    $('#ma_grant_date').val(data['ma_grant_date']);
+    $('#ma_renewal_date').val(data['ma_renewal_date']);
+    $('#remarks').val(data['remarks']);
+    let s = '';
+    for (let val of data['file_list']) {
+        s = `${s} <b>${val['type']}</b>: ${val['name']}<br>`;
+    }
+    $('#file_list').html(s);
+
+    $('#result').text(JSON.stringify(data,null,2));
+    $('#user').text(JSON.stringify( {uid: credentials.user.uid, email: credentials.user.email}, null, 2));
+    $('#data').text(JSON.stringify( data, null, 2));
+
+    await configIssueCollector(config['issueCollector'], window, data);
+
+    $('#result').html('<b>OK:</b> data fetched correctly'); 
+
+}
+
+function getData(page_id, spaceKey, baseUrl, token){
+    let URL = baseUrl + '/documents/' + page_id + '.json?auth=' + token;
     console.log(URL);
     return $.ajax(URL, {
         type: 'GET,PUT,POST,DELETE',
@@ -103,28 +74,37 @@ function getData(doc_id, baseUrl, token){
         }
     })
     .then( (response) => { return response;})
-    .then( (data) =>  { return data; })
+    .then( (data) =>  { 
+        data['page_id'] = page_id;
+        data['spaceKey'] = spaceKey;
+        return data;
+     })
     .catch( (error) => {throw error} );
 }
 
-function renderData(data) {
-    $('#product_name').val(data['product_name']);
-    $('#ma_renewal_date').val(data['ma_renewal_date']);
-    $('#pack_size').val(data['pack_size']);
-    $('#stability').val(data['stability']);
-    $('#storage_conditions').val(data['storage_conditions']);
-    $('#country').val(data['country']);
-    $('#procedure_number').val(data['procedure_number']);
-    $('#ma_number').val(data['ma_number']);
-    $('#ma_grant_date').val(data['ma_grant_date']);
-    $('#desc').val(data['desc']);
-    $('#remarks').val(data['remarks']);
-    let s = '';
-    for (let val of data['file_list']) {
-        s = `${s} <b>${val['type']}</b>: ${val['name']}<br>`;
-    }
-    $('#file_list').html(s);
-    $('#result').text(JSON.stringify(data,null,2));
+function configIssueCollector(colloectorUrl, window, data) {
+    $.ajax({
+        url: colloectorUrl, 
+        type: 'GET',
+        async: false,
+        cache: true,
+        dataType: 'script',
+        success: () => window.ATL_JQ_PAGE_PROPS = {
+                "triggerFunction": function(showCollectorDialog) {
+                    jQuery("#change").click(function(e) {
+                        e.preventDefault();
+                        showCollectorDialog();
+                    });
+                },
+                fieldValues: {
+                    summary : 'Change request for ' + data['product_name'],
+                    customfield_10195 : data['page_id'],
+                    customfield_10194 : data['spaceKey']
+                }
+            }
+        ,
+        error: (xhr, textStatus, error) => {throw error;}
+    });
 }
 
-export {htmlContent, initialize, signin, getData, getToken, renderData};
+export { renderPage };
