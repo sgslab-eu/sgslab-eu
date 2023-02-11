eb4087e81ec3d1991b6ddc68b3c820d1b9ac9b51
sgslab-eu <124488523+sgslab-eu@users.noreply.github.com>
2023-02-11 12:07:50 +0000

    _

--- src/js/script.js
+++ src/js/script.js
@@ -44,8 +44,9 @@ async function renderPage(config, page_id, spaceKey, window) {
     $('#remarks').val(data['remarks']);
     let s = '';
     let f = [];
+    downloadUrl = 'https://sgslab.atlassian.net/wiki/download/attachments/' + page_id
     for (let val of data['file_list']) {
-        s = `${s} <b>${val['type']}</b>: ${val['name']}<br>`;
+        s = `${s} <b>${val['type']}</b>: <a href="${downloadUrl}/${val['name']}?api=v2">${val['name']}</a><br>`
         f.push(val['name']);
     }
     $('#file_list').html(s);
